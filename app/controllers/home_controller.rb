71a9047834f22c193cf2cffaca41553d1df00f2d
Sidra <alice@alum.mit.edu>
2010-12-16 19:55:30 -0500

    made home page more useful during demo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @users = User.all
-    @volunteers = Role.find_or_create_by_name(:support_volunteer).users
     @support_admins = Role.find_or_create_by_name(:support_admin).users
+    @volunteers = Role.find_or_create_by_name(:support_volunteer).users - @support_admins
+    @users = User.all - @volunteers - @support_admins
   end
 
   def support
